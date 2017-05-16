4976c3592de15215de94cb279ff9ca8788699855
merturl <whdgus963@naver.com>
2017-05-17 04:18:59 +0900

    게임뷰 업데이트 및 이미지 수정, 카드드로우,카드플립 추가

--- Server/public/javascripts/drowcard.js
+++ Server/public/javascripts/drowcard.js
@@ -1,98 +1,82 @@
 $(function(){
-    var direction = {left: "+=60%"}
-    var directionCount = 0
-    var moveLeftCount = 0
+    var direction = {left: "+=75%", bottom: "+=22%"}
+    var directionCount = 0 //주는 카드 방향 지정
+    var moveLeftCount = 0 // 받는 카드를 세장씩 정렬
     var drawCards = $('.cards').each(function(index) {
-                        $(this).delay(300*index).animate(direction)
+                        $(this).delay(1*index).animate(direction)
                         if(directionCount == 0){
-                            if(moveLeftCount == 0 ){
-                                direction = {bottom: "-=60%"}
+                            if(moveLeftCount == 0 ){// 2번 플레이어
+                                direction = {left: "+=75%", bottom: "-=35%"}
                                 directionCount++
                             }else if(moveLeftCount ==1){
-                                direction = {bottom: "-=60%", left: "+=10%"}
+                                direction = {left: "+=65%", bottom: "-=35%"}
                                 directionCount++
                             }else if(moveLeftCount ==2){
-                                direction = {bottom: "-=60%", left: "+=20%"}
+                                direction = {left: "+=55%", bottom: "-=35%"}
                                 directionCount++
                             }
-                        }else if(directionCount == 1){
+                        }else if(directionCount == 1){ //3번 플레이어
                             if(moveLeftCount == 0 ){
-                                direction = {left: "-=80%"}
+                                direction = {left: "-=65%", bottom: "-=35%"}
                                 directionCount++
                             }else if(moveLeftCount ==1){
-                                direction = {left: "-=70%"}
+                                direction = {left: "-=55%", bottom: "-=35%"}
                                 directionCount++
                             }else if(moveLeftCount ==2){
-                                direction = {left: "-=60%"}
+                                direction = {left: "-=45%", bottom: "-=35%"}
                                 directionCount++
                             }
-                        }else if(directionCount == 2){
+                        }else if(directionCount == 2){//4번 플레이어
                             if(moveLeftCount == 0 ){
-                                direction = {bottom: "+=60%"}
+                                direction = {left: "-=65%", bottom: "+=22%"}
                                 directionCount++
                             }else if(moveLeftCount ==1){
-                                direction = {bottom: "+=60%", left: "+=10%"}
+                                direction = {left: "-=55%", bottom: "+=22%"}
                                 directionCount++
                             }else if(moveLeftCount ==2){
-                                direction = {bottom: "+=60%", left: "+=20%"}
+                                direction = {left: "-=45%", bottom: "+=22%"}
                                 directionCount++
                             }
                         }
-                        else if(directionCount == 3){
+                        else if(directionCount == 3){//1번 플레이어
                             if(moveLeftCount == 0 ){
-                                direction = {left: "+=70%"}
+                                direction = {left: "+=65%", bottom: "+=22%"}
                                 directionCount = 0
                                 moveLeftCount++
                             }else if(moveLeftCount ==1){
-                                direction = {left: "+=80%"}
+                                direction = {left: "+=55%", bottom: "+=22%"}
                                 directionCount = 0
                                 moveLeftCount++
                             }
                         }
     });
+
     $.when(drawCards).then(function (){
-        $(".img:nth-child(3)").flip();
-
-        // $(".img:nth-child(1)").fadeIn().queue(function() {
-        //     $(".img:nth-child(2)").fadeIn().queue(function() {
-        //         $(".img:nth-child(3)").fadeIn();
-        //     });
-        // });
+        $(".moneypanel").fadeIn();
+        $(".cards").transition({ 
+            perspective: '100px',
+            rotateY: '180deg'
+        });
     });
-});
 
-
-//left: 534
-
-
-// $(function(){
-//     var direction = {left: "+=60%"}
-//     var directionCount = 0
-//     $('.cards').each(function(index) {
-//         $(this).delay(200*index).animate(direction,{complete: function(){
-//             $(".img:nth-child(1)").fadeIn().queue(function() {
-//                 $(".img:nth-child(2)").fadeIn().queue(function() {
-//                     $(".img:nth-child(3)").fadeIn();
-//                 });
-//             });
-//         }});
-//         if(directionCount == 0){
-//             direction = {bottom: "-=60%"}
-//             directionCount++
-//         }else if(directionCount == 1){
-//             direction = {left: "-=60%"}
-//             directionCount++
-//         }else if(directionCount == 2){
-//             direction = {bottom: "+=60%"}
-//             directionCount++
-//         }
-//         else if(directionCount == 3){
-//             direction = {left: "+=60%"}
-//             directionCount =0
-//         }
-//     });
-// });
-
-
-
-// //left: 534
+    $.when(drawCards).then(function (){
+         $(".mycards").fadeIn();
+          var myCardsDirection = 0
+          var myCardsDirectionCounter = 0;
+            var checkMyCards = $('.mycards').each(function(index) {
+                $(this).delay(1*index).animate(myCardsDirection);
+                if(myCardsDirectionCounter == 0 ){
+                    myCardsDirection = {left: "+=8%"}
+                    myCardsDirectionCounter++
+                }else if(myCardsDirectionCounter ==1){
+                    myCardsDirection = {left: "+=16%"}
+                }
+            });
+            $.when(checkMyCards).then(function (){
+                $(".mycards").transition({ 
+                    perspective: '100px',
+                    rotateY: '180deg'
+                });
+            });
+        });
+    });
