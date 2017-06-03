7c976ef1cbf640b0f9445332a1c4e900cd20171a
merturl <whdgus963@naver.com>
2017-06-03 19:18:43 +0900

    server- 돈을 다 모으고 가져가게 수정, 돈 뭉치 html을 hide가 아닌 remove로 변경

--- Server/public/javascripts/bettingmoney.js
+++ Server/public/javascripts/bettingmoney.js
@@ -14,30 +14,32 @@ function firstBetting(i){
 }
 
     //각 플레이어의 배팅이 끝난 돈을 센터로 모은다.
-    function combineMoney(player){//각 플레이어의 배팅이 다 되었다면 돈을 센터로
+    function combineMoney(player){//각 플레이어의 배팅이 다 되었다면 돈을 센터로 
         var collectCenter1=$('.bettingMoney1').animate({left: "-=10%", top:"+=210%"});  //1번 플레이어 돈 센터로
         var collectCenter2=$('.bettingMoney2').animate({left: "-=10%", top:"-=570%"}); //2번 플레이어 돈 센터로
         var collectCenter3=$('.bettingMoney3').animate({left: "+=9%", top:"-=570%"}); //3번 플레이어 돈 센터로
         var collectCenter4=$('.bettingMoney4').animate( {left: "+=9%" ,top:"+=210%"}); //4번 플레이어 돈 센터로
-        if(player === 1){ //1번 플레이어 
-            var winDirection = {left: "+=43%" , top: "-=210%"};
-            $.when($('.parentMoney').children().animate(winDirection)).then(function (){
-                $('.parentMoney').children().hide();
-            });
-        }else if(player ===2){//2번 플레이어
-            var winDirection = {left: "+=43%", top:"+=570%"};
-            $.when($('.parentMoney').children().animate(winDirection)).then(function (){
-                $('.parentMoney').children().hide();
-            });
-        }else if(player ===3){//3번 플레이어
-            var winDirection = {left: "-=43%", top: "+=570%"};
-            $.when($('.parentMoney').children().animate(winDirection)).then(function (){
-                $('.parentMoney').children().hide();
-            });
-        }else if(player ===4){//4번 플레이어
-            var winDirection = {left: "-=43%",  top: "-=210%"};
-            $.when($('.parentMoney').children().animate(winDirection)).then(function (){
-                $('.parentMoney').children().remove();
-            });
-        }
+        var collectMoney =  $.when(collectCenter1,collectCenter2,collectCenter3,collectCenter4).then(function (){
+            if(player === 1){ //1번 플레이어
+                var winDirection = {left: "+=43%" , top: "-=210%"};
+                $.when($('.parentMoney').children().animate(winDirection)).then(function (){
+                    $('.parentMoney').children().remove();
+                });
+            }else if(player ===2){//2번 플레이어
+                var winDirection = {left: "+=43%", top:"+=570%"};
+                $.when($('.parentMoney').children().animate(winDirection)).then(function (){
+                    $('.parentMoney').children().remove();
+                });
+            }else if(player ===3){//3번 플레이어
+                var winDirection = {left: "-=43%", top: "+=570%"};
+                $.when($('.parentMoney').children().animate(winDirection)).then(function (){
+                    $('.parentMoney').children().remove();
+                });
+            }else if(player ===4){//4번 플레이어
+                var winDirection = {left: "-=43%",  top: "-=210%"};
+                $.when($('.parentMoney').children().animate(winDirection)).then(function (){
+                    $('.parentMoney').children().remove();
+                });
+            }
+        });
     }
